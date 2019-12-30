9c38acaeb083acb5f7fa37d4ce15e1e43bcb0bd0
Andrea Bender <andreab.1726@gmail.com>
2019-12-30 09:39:37 -0700

    updated user-endpoint

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -21,16 +21,16 @@ VALUES
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'Quel re', 'Good day', 2),
-  (2, 1, 'Mellon', 'Friend', 3),
-  (3, 1, 'Dina', 'Be silent', 4),
-  (4, 1, 'Thalias', 'Bravery', 5),
-  (5, 1, 'Namárië', 'Farewell', 6),
-  (6, 1, 'Parma', 'Book', 7),
-  (7, 1, 'Áre', 'Sunlight', 8),
-  (8, 1, 'Beleg', 'Mighty', 9),
-  (9, 1, 'Tinco', 'Metal', 10),
-  (10, 1, 'Amarth', 'Doom', null);
+  (1, 1, 'quel re', 'good day', 2),
+  (2, 1, 'mellon', 'friend', 3),
+  (3, 1, 'dina', 'be silent', 4),
+  (4, 1, 'thalias', 'bravery', 5),
+  (5, 1, 'namárië', 'farewell', 6),
+  (6, 1, 'parma', 'book', 7),
+  (7, 1, 'áre', 'sunlight', 8),
+  (8, 1, 'beleg', 'mighty', 9),
+  (9, 1, 'tinco', 'metal', 10),
+  (10, 1, 'amarth', 'doom', null);
 
 
 UPDATE "language" SET head = 1 WHERE id = 1;
