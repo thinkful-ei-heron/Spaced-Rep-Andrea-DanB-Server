8660170e826251929d0cfa7f43143f53a9501bf6
Daniel Bright <brahyt@gmail.com>
2020-01-02 14:03:13 -0800

    merge stuff

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -4,24 +4,26 @@ const jwt = require('jsonwebtoken');
 const config = require('../config');
 
 const AuthService = {
-	getUserWithUserName(db, username) {
-		return db('users').where({ username }).first();
-	},
-	comparePasswords(password, hash) {
-		return bcrypt.compare(password, hash);
-	},
-	createJwt(subject, payload) {
-		return jwt.sign(payload, config.JWT_SECRET, {
-			subject,
-			expiresIn: config.JWT_EXPIRY,
-			algorithm: 'HS256'
-		});
-	},
-	verifyJwt(token) {
-		return jwt.verify(token, config.JWT_SECRET, {
-			algorithms: [ 'HS256' ]
-		});
-	}
+  getUserWithUserName(db, username) {
+    return db('users')
+      .where({username})
+      .first();
+  },
+  comparePasswords(password, hash) {
+    return bcrypt.compare(password, hash);
+  },
+  createJwt(subject, payload) {
+    return jwt.sign(payload, config.JWT_SECRET, {
+      subject,
+      expiresIn: config.JWT_EXPIRY,
+      algorithm: 'HS256',
+    });
+  },
+  verifyJwt(token) {
+    return jwt.verify(token, config.JWT_SECRET, {
+      algorithms: ['HS256'],
+    });
+  },
 };
 
 module.exports = AuthService;
